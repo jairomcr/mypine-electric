3da86b497ee2d45924aeb8d3c435ed18dbc93e15
jairomcr <jairocascaretrodriguez@gmail.com>
2024-03-26 11:53:32 -1200

    addEmail config

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -86,4 +86,16 @@ class AuthController extends BaseController
             
         }
     }
+    public function forgotForm() 
+    {
+        $data = array(
+            'pageTitle'=>'Forgot password',
+            'validation'=>null,
+        );
+        return view('backend/pages/auth/forgot',$data);
+    }
+    public function sendPasswordReset()
+    {
+        
+    }
 }
